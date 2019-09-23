cdd91682d49a83ca28961f2600b801ef170a4711
wuwei7 <wuwei7@staff.weibo.com>
2019-09-23 18:40:55 +0800

    server2

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config(object):
     #vocab_path = '/Users/Zealot/yizhou/yizhou/git/dssm/data/vocab.txt'
     vocab_path = './data/vocab.txt'
     # vocab_path = './data/vocab_filtered.txt'
-   # file_train = './data/oppo_round1_train_20180929_mini_test.txt'
+    #file_train = './data/oppo_round1_train_20180929_mini_test.txt'
     file_train = './data/oppo_round1_train_20180929.txt'
     #file_train = './data/oppo_round1_train_20180929_2.txt'
     #file_vali = './data/oppo_round1_vali_20180929_mini.txt'
@@ -31,13 +31,13 @@ class Config(object):
     use_stack_rnn = False
     learning_rate = 0.01
     # max_steps = 8000
-    num_epoch = 5
+    num_epoch = 4
     summaries_dir = './Summaries/'
     gpu = 0
     # negative sample
     NEG = 4
     # query batch size
-    query_BS = 1024
+    query_BS = 256
     query_mid_vector_file = r'data/y_mid_vector.txt'
     doc_pos_y_mid_vector_file = r'data/doc_pos_y_mid_vector.txt'
     doc_neg_y_mid_vector_file = r'data/doc_neg_y_mid_vector.txt'
