393aac0f7ce6438c853b175cf69eaeed54aba483
MC-Zealot <263097241@qq.com>
2019-10-12 18:10:26 +0800

    comment change

--- config.py
+++ config.py
@@ -21,10 +21,12 @@ class Config(object):
     #vocab_path = '/Users/Zealot/yizhou/yizhou/git/dssm/data/vocab.txt'
     vocab_path = './data/vocab.txt'
     # vocab_path = './data/vocab_filtered.txt'
-    file_train = './data/oppo_round1_train_20180929_mini_test.txt'
+    # file_train = './data/oppo_round1_train_20180929_mini_test.txt'
+    file_train = './data/comment/dataset20190101_no_extend_no_left.txt'
     # file_train = './data/oppo_round1_train_20180929.txt'
     #file_train = './data/oppo_round1_train_20180929_2.txt'
-    file_vali = './data/oppo_round1_vali_20180929_mini.txt'
+    # file_vali = './data/oppo_round1_vali_20180929_mini.txt'
+    file_vali = './data/comment/dataset20190101_no_extend_no_left_vali.txt'
     # file_vali = './data/oppo_round1_vali_20180929.txt'
     max_seq_len = 10
     hidden_size_rnn = 100
