f2d9704833ffc634dc44000c0f856f4d19074382
MC-Zealot <263097241@qq.com>
2019-10-22 16:29:31 +0800

    add pre process to remove special chars

--- utils.py
+++ utils.py
@@ -257,9 +257,9 @@ def str_to_dict(str):
 
 def test_case_for_cal_similarity():
     # 1、打开query文件，加载数据到list[dict]中，
-    # 2、打开doc_neg文件，加载数据到list[dict]中，
-    # 3、根据index选择query-docs
-    # 4、分别计算相似度并且打分，打印出来
+    #     # 2、打开doc_neg文件，加载数据到list[dict]中，
+    #     # 3、根据index选择query-docs
+    #     # 4、分别计算相似度并且打分，打印出来
     query_list = []
     doc_list = []
     query_file_name = conf.query_mid_vector_file
@@ -324,18 +324,23 @@ def get_data_set_comment(FileName):
     doc_neg = []
     with open(FileName, encoding='utf8') as f:
         for line in f.readlines():
-            line = pre_process(line)
+
             spline = line.split('\t')
             if len(spline) < 3:
                 continue
             prefix,  title, label = spline
             if label == '0':
                 continue
+            prefix = pre_process(prefix)
+            title = pre_process(title)
+
             prefix = [i for i in prefix]
             title = [i for i in title]
             prefix = " ".join(prefix)
             title = " ".join(title)
+
             query.append(prefix)
+            print(query)
             doc.append(title)
             # doc_neg.extend(cur_arr[:conf.NEG])
     size = len(doc)
@@ -361,23 +366,26 @@ def pre_process(line):
         line = line.replace(urls[0], "")
         for index in range(len(urls)):
             line = line.replace(urls[index], "")
+    line = re.sub(u"([^\u4e00-\u9fa5\u0030-\u0039\u0041-\u005a\u0061-\u007a])", "", line)
     return line
 
 
 if __name__ == '__main__':
     print("hello")
-    pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')  # 匹配模式
+    # vectorizer=load_vectorizer()
+    # print(vectorizer.get_feature_names())
+    # pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')  # 匹配模式
 
     # str = 'Its after 12 noon, do you know where your rooftops are? '
-    str = '高速车震……速看！！！！http://t.cn/RSNVzD8 ​​	  [馋嘴]玩了几天，这个月的花贝都还上了[弗莱见钱眼开]缺零花钱的可以来玩一下[来] http://t.cn/Eo3OKsf [弗莱见钱眼开][弗莱见钱眼开][弗莱见钱眼开][弗莱见钱眼开]玩每天赚点新闻赚钱辣么多零花钱随便你赚啦[嘻嘻][嘻嘻]	1	4373771843879773	4370362685017799 '
-    print(str)
+    # str = '高速车震……速看！！！！http://t.cn/RSNVzD8 ​​	  [馋嘴]玩了几天，这个月的花贝都还上了[弗莱见钱眼开]缺零花钱的可以来玩一下[来] http://t.cn/Eo3OKsf [弗莱见钱眼开][弗莱见钱眼开][弗莱见钱眼开][弗莱见钱眼开]玩每天赚点新闻赚钱辣么多零花钱随便你赚啦[嘻嘻][嘻嘻]	1	4373771843879773	4370362685017799 '
+    # print(str)
     # url = re.findall(pattern, str)
     # print(url,len(url))
     # str=str.replace(url[0],"")
-    print(pre_process(str))
+    # print(pre_process(str))
     # test_case_for_cal_similarity()
-    # file_train = './data/comment/dataset20190101_no_extend_no_left.txt'
-    # query, doc, doc_neg = get_data_set_comment(file_train)
+    file_train = './data/comment/dataset20190101_no_extend_no_left.txt'
+    query, doc, doc_neg = get_data_set_comment(file_train)
     # idx = 4
     # print(query[idx])
     # print(doc[idx])
